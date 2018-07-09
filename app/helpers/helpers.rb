99af1ac6872c6f120f6fe8bbfc7ed30c70e5d0ce
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-07-09 16:03:25 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,12 +1,6 @@
 class Helpers
 
   def self.current_user(arg)
-    @user = User.find_by(arg[:user_id].to_s)
-    @user
+    User.find_by(session[:user_id])
   end
-
-  def self.is_logged_in?(arg)
-    true if !!arg[:user_id]
-  end
-
 end
