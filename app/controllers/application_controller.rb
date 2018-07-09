ccf64ea4a6e4784c366e400e3252980717b76c4e
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-07-09 17:37:10 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,7 +25,7 @@ class ApplicationController < Sinatra::Base
     @current_user = User.find_by(session[:user_id].to_s)
 
     if !@current_user
-      redirect '/error'
+      erb :/error'
     end
     erb :account
   end
