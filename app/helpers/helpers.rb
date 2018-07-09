5d43c2d006e11271f6b2611db84fbc2ee2e3129e
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-07-09 16:07:48 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,8 +1,8 @@
 class Helpers
 
   def self.current_user(arg)
-    user = User.find_by(arg[:user_id])
-    user
+    @user = User.find_by(arg[:user_id])
+    @user
   end
 
   def self.is_logged_in?(arg)
