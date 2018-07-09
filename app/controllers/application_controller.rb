fde4dfdfdddd65db153bc487df9be221b21a4c6f
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-07-09 16:52:07 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,7 @@ class ApplicationController < Sinatra::Base
 
   get '/account' do
     @current_user = User.find_by(session[:user_id])
-    @current_user.to_s
+    @current_user.id.to_s
     #if @current_user
     #  erb :account
     #else
